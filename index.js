30a81e3ea9e74f4867090654488966e3f4b86363
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-15 20:33:29 +0530

    default structure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HomePage from './src/containers/HomePage';
 
-const App = () => <div>Hello World</div>
+const App = () => <div><HomePage /></div>
 
 ReactDOM.render(<App />, document.getElementById('app'));
