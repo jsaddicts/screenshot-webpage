ff3da69d47038adb48c11ccabc25c4c884dded57
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-15 18:58:21 +0530

    added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	entry: [
+		__dirname + '/index.js'
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},{
+				test: /\.(less|css)$/,
+				loaders: [
+					"style-loader",
+					"css-loader",
+					"less-loader",
+				]
+			}
+		]
+	},
+	output: {
+		path: __dirname + '/static',
+		publicPath: '/static',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: __dirname + '/dist',
+		historyApiFallback: true
+	}
+}
