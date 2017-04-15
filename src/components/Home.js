a4feb8123151d9c54303a394a58ff01d22f8a80e
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-15 21:29:48 +0530

    create full workflow

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,37 @@
 import React, {Component} from 'react';
 import {Row, Col, FormInput, Button, InputGroup} from 'elemental';
+import {takePicture} from '../utils';
 
 class Home extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			params: {
+				url: 'http://www.google.com',
+				width: 1360,
+				height: 768
+			}
+		}
+	}
+
+	onSubmit = () => {
+		takePicture(this.state.params)
+			.then(res => console.log(res))
+			.catch(err => console.log(err))
+	}
+
+	updateValue = (e) => {
+		this.setState({
+			params: {
+				...this.state.params,
+				[e.target.name]: e.target.value
+			}
+		})
+	}
+
 	render () {
+		const {url, width, height} = this.state.params;
 		return (
 			<div>
 				<Row>
@@ -10,15 +39,15 @@ class Home extends Component {
 					<Col sm="1/3" className="text-center container">
 						<InputGroup>
 							<InputGroup.Section grow>
-								<FormInput autoFocus type="text" placeholder="Webpage Url" />
+								<FormInput name="url" value={url} onChange={this.updateValue} autoFocus type="text" placeholder="Webpage Url" />
 							</InputGroup.Section>
 						</InputGroup>
 						<InputGroup>
 							<InputGroup.Section grow>
-								<FormInput autoFocus type="text" placeholder="Width" />
+								<FormInput name="width" value={width} onChange={this.updateValue} autoFocus type="text" placeholder="Width" />
 							</InputGroup.Section>
 							<InputGroup.Section grow>
-								<FormInput autoFocus type="text" placeholder="Height" />
+								<FormInput name="height" value={height} onChange={this.updateValue} autoFocus type="text" placeholder="Height" />
 							</InputGroup.Section>
 							<InputGroup.Section>
 								<Button type="success">Take Picture</Button>
