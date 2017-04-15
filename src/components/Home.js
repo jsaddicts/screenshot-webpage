53129ebbc481e1aafb66222072cc9ec3193d6142
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-15 21:16:18 +0530

    added width & height, removed heading

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Row, Col} from 'elemental';
+import {Row, Col, FormInput, Button, InputGroup} from 'elemental';
 
 class Home extends Component {
 	render () {
@@ -7,10 +7,23 @@ class Home extends Component {
 			<div>
 				<Row>
 					<Col sm="1/3"></Col>
-					<Col sm="1/3">
-						<h1 className="text-center main-heading">
-							Take A Picture
-						</h1>
+					<Col sm="1/3" className="text-center container">
+						<InputGroup>
+							<InputGroup.Section grow>
+								<FormInput autoFocus type="text" placeholder="Webpage Url" />
+							</InputGroup.Section>
+						</InputGroup>
+						<InputGroup>
+							<InputGroup.Section grow>
+								<FormInput autoFocus type="text" placeholder="Width" />
+							</InputGroup.Section>
+							<InputGroup.Section grow>
+								<FormInput autoFocus type="text" placeholder="Height" />
+							</InputGroup.Section>
+							<InputGroup.Section>
+								<Button type="success">Take Picture</Button>
+							</InputGroup.Section>
+						</InputGroup>
 					</Col>
 					<Col sm="1/3"></Col>
 				</Row>
