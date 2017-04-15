f2f5afefae0bb399340afad7caf2db3d38b67554
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-15 20:41:51 +0530

    added elemental

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './src/containers/HomePage';
 
+import 'elemental/less/elemental.less';
+
 const App = () => <div><HomePage /></div>
 
 ReactDOM.render(<App />, document.getElementById('app'));
